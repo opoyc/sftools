74a9f9bd3bd9e7661dc2e68bee857826581145cf
opoyc <opoyserc@gmail.com>
2020-02-13 15:36:20 +0100

    to object

--- R/load_gbu.R
+++ R/load_gbu.R
@@ -5,6 +5,8 @@
 #' @return tibble
 #' @author Obryan Poyser
 #'
+#' @importFrom dplyr as_tibble
+#'
 #' @examples
 #' \dontrun{
 #' load_gbu()
@@ -18,7 +20,7 @@ load_gbu <- function(gbu_path, on_globalenv=F){
     names(local_env$GBU) <- c("market", "gmid", "activity", "activity_pfwd", "gbu")
     local_env$GBU$key <- paste0(strtrim(local_env$GBU$market, 2), ": ", local_env$GBU$gmid)
     gbu <- local_env$GBU[c("key", "gmid", "gbu")]
-    gbu <- gbu[gbu$key!="",]
+    gbu <- as_tibble(gbu[nchar(gbu$key)>5,])
     if(on_globalenv==T){
         gbu <<- gbu
     } else {
